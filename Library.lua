a531d844d746e5a49d3884c1e4e513badc53421f
QuantumPsyche <89321433+QuantumPsyche@users.noreply.github.com>
2022-11-30 21:11:00 -0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2661,9 +2661,9 @@ function Library:CreateWindow(...)
             local GroupboxLabel = Library:CreateLabel({
                 Size = UDim2.new(1, 0, 0, 18);
                 Position = UDim2.new(0, 4, 0, 2);
-                TextSize = 16;
+                TextSize = 14;
                 Text = Info.Name;
-                TextXAlignment = Enum.TextXAlignment.Center;
+                TextXAlignment = Enum.TextXAlignment.Left;
                 ZIndex = 5;
                 Parent = BoxInner;
             });
